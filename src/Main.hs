8ee487368c58c9c3fb2b19394b6f016e026b4eb1
Peter Jones <pjones@pmade.com>
2013-02-06 18:17:09 -0700

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,15 @@
+{-
+
+This file is part of the Haskell package vimeta. It is subject to the
+license terms in the LICENSE file found in the top-level directory of
+this distribution and at git://pmade.com/vimeta/LICENSE. No part of
+vimeta package, including this file, may be copied, modified,
+propagated, or distributed except according to the terms contained in
+the LICENSE file.
+
+-}
+
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello World"
