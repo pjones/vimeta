4172e16975bb58802058d7778b9b3926b591a083
Peter Jones <pjones@devalot.com>
2019-10-13 16:46:25 -0700

    Update dependencies and build system

--- default.nix
+++ default.nix
@@ -2,35 +2,32 @@
 }:
 
 let
-  # Extra system dependencies needed:
-  extraPackages = with pkgs; [ atomicparsley ];
-
-  # Grab the latest version of some dependencies:
-  themoviedb = fetchGit "https://code.devalot.com/open/themoviedb.git";
-  byline = fetchGit "https://code.devalot.com/open/byline.git";
-
-  # Helpful if you want to override any Haskell packages:
-  overrides = self: super: {
-    http-client = if super ? http-client_0_6_2
-      then super.http-client_0_6_2
-      else super.http-client;
-
-    byline = import "${byline}/default.nix" { inherit pkgs; };
-    themoviedb = import "${themoviedb}/default.nix" { inherit pkgs; };
+  nix-hs-src = fetchGit {
+    url = "https://code.devalot.com/open/nix-hs.git";
+    rev = "a2b666faf8cb3c6f769655dfb36f4695f78bc3c3";
   };
 
-  # Apply the overrides from above:
-  haskell = pkgs.haskellPackages.override (orig: {
-    overrides = pkgs.lib.composeExtensions
-      (orig.overrides or (_: _: {})) overrides; });
-in
+  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
 
-# Load the local nix file and use the overrides from above:
-haskell.callPackage ./vimeta.nix {
-  mkDerivation = { buildTools ? []
-                 , ...
-                 }@args:
-    haskell.mkDerivation (args // {
-      buildTools = buildTools ++ extraPackages;
-    });
+in nix-hs {
+  cabal = ./vimeta.cabal;
+
+  buildInputs = with pkgs; [ atomicparsley ];
+
+  overrides = lib: self: super: with lib; {
+    byline = lib.fetchGit {
+      url = "https://code.devalot.com/open/byline.git";
+      rev = "f2e6eb1926873433129d3103efe11090b3f1b514";
+    };
+
+    themoviedb = lib.fetchGit {
+      url = "https://code.devalot.com/open/themoviedb.git";
+      rev = "4aeb946d5b25754eab5daaab1ba0f269f03a7c59";
+    };
+
+    optparse-applicative =
+      if super ? optparse-applicative_0_15_0_0
+        then super.optparse-applicative_0_15_0_0
+        else optparse-applicative;
+  };
 }
