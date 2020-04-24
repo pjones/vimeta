2b345a3bc5bbdc47e0301a67a7c9569e96d4092d
Peter Jones <pjones@devalot.com>
2020-04-23 17:14:14 -0700

    Update build system

--- default.nix
+++ default.nix
@@ -1,29 +1,26 @@
-{ pkgs ? import <nixpkgs> { }
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs {}
+, nix-hs ? import sources.nix-hs { inherit pkgs; }
+, byline ? sources.byline
+, themoviedb ? sources.themoviedb
+, ghcide ? sources.ghcide-nix
+, ormolu ? sources.ormolu
 }:
 
-let
-  nix-hs-src = fetchGit {
-    url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "a2b666faf8cb3c6f769655dfb36f4695f78bc3c3";
-  };
-
-  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
-
-in nix-hs {
+nix-hs {
   cabal = ./vimeta.cabal;
 
   buildInputs = with pkgs; [ atomicparsley ];
 
   overrides = lib: self: super: with lib; {
-    byline = lib.fetchGit {
-      url = "https://code.devalot.com/open/byline.git";
-      rev = "f2e6eb1926873433129d3103efe11090b3f1b514";
-    };
+    byline = import byline { inherit (lib) pkgs; };
+    themoviedb = import themoviedb { inherit (lib) pkgs; };
+    ghcide = import ghcide {};
 
-    themoviedb = lib.fetchGit {
-      url = "https://code.devalot.com/open/themoviedb.git";
-      rev = "4aeb946d5b25754eab5daaab1ba0f269f03a7c59";
-    };
+    ormolu = (import ormolu {
+      inherit (lib) pkgs;
+      ormoluCompiler = lib.compilerName;
+    }).ormolu;
 
     optparse-applicative =
       if super ? optparse-applicative_0_15_0_0
