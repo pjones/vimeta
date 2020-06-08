a357a3441f914e0bfc40bfc332a215dfbb13e073
Peter Jones <pjones@devalot.com>
2020-06-08 13:27:24 -0700

    Revert compiler attribute

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
 { sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
-, nix-hs ? import sources.nix-hs { inherit pkgs; }, compiler ? "default" }:
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, ghc ? "default" }:
 
 nix-hs {
-  inherit compiler;
-
   cabal = ./vimeta.cabal;
   buildInputs = with pkgs; [ atomicparsley ];
+  compiler = ghc;
 }
