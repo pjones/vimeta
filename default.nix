32d2742f8095aa40e6107c1ad8422aca4440d426
Peter Jones <pjones@devalot.com>
2020-06-08 13:06:09 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,27 +1,9 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs {}
-, nix-hs ? import sources.nix-hs { inherit pkgs; }
-, byline ? sources.byline
-, themoviedb ? sources.themoviedb
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
-, ghc ? "default"
-}:
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }, compiler ? "default" }:
 
 nix-hs {
+  inherit compiler;
+
   cabal = ./vimeta.cabal;
-  compiler = ghc;
-
   buildInputs = with pkgs; [ atomicparsley ];
-
-  overrides = lib: self: super: with lib; {
-    byline = import byline { inherit (lib) pkgs; };
-    themoviedb = import themoviedb { inherit (lib) pkgs; };
-    ghcide = import ghcide {};
-
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-  };
 }
