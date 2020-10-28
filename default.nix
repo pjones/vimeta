2f0214e1bc45e05c7b597a7c21bd50064acbb891
Peter Jones <pjones@devalot.com>
2020-10-28 10:45:25 -0700

    Update dependencies, prepare for release

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 { sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
+, nixpkgs ? "nixpkgs"
+, pkgs ? import sources.${nixpkgs} { }
 , nix-hs ? import sources.nix-hs { inherit pkgs; }
 , ghc ? "default"
 }:
@@ -26,15 +27,30 @@ nix-hs {
       then super.aeson_1_5_2_0
       else super.aeson;
 
+    ansi-terminal =
+      if super ? ansi-terminal_0_11
+      then super.ansi-terminal_0_11
+      else super.ansi-terminal;
+
     http-client =
       if super ? http-client_0_7_2_1
       then super.http-client_0_7_2_1
       else super.http-client;
 
-    # ERROR: infinite recursion encountered, at undefined position
-    # time =
-    #   if super ? time_1_10
-    #   then super.time_1_10
-    #   else super.time;
+    haskeline = lib.dontCheck (
+      if super ? haskeline_0_8_1_0
+      then lib.dontCheck super.haskeline_0_8_1_0
+      else super.haskeline
+    );
+
+    optparse-applicative =
+      if super ? optparse-applicative_0_16_0_0
+      then super.optparse-applicative_0_16_0_0
+      else super.optparse-applicative;
+
+    relude =
+      if super ? relude_0_6_0_0
+      then super.relude_0_6_0_0 # NixOS 20.03
+      else super.relude;
   };
 }
