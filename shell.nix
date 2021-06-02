8c24ebb4f8bd0926cca6cab32db34820121e0bde
Peter Jones <pjones@devalot.com>
2021-06-02 14:54:12 -0700

    Update dependencies and build w/ GHC 9.0.1

--- shell.nix
+++ shell.nix
@@ -1,5 +1,3 @@
-{ ghc ? "default"
-}:
 let
   sources = import nix/sources.nix;
   pkgs = import sources.nixpkgs { };
@@ -11,7 +9,7 @@ let
 in
 # Load an interactive environment:
 (import ./. {
-  inherit ghc sources pkgs;
+  inherit sources pkgs;
 }).interactive.overrideAttrs (_: {
   # Export a good NIX_PATH for tools that run in this shell.
   NIX_PATH = lib.concatStringsSep ":"
